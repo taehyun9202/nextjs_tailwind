0d06052f7581051eeaa7b23b86ce6d2b7dd3da6a
Tyler <taehyun9202@hotmail.com>
2021-04-21 16:17:01 -0400

    final commit

--- keys.js
+++ keys.js
@@ -1,5 +1,4 @@
 // api key - https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = "AIzaSyCPvdEn7ul6l4c1qIW4eCR8W_z8UYgfIMQ";
+export const API_KEY = process.env.REACT_APP_API_KEY;
 // context key - https://cse.google.com/cse/create/new
-
-export const CONTEXT_KEY = "621f2b4cbcf0ee0e8";
+export const CONTEXT_KEY = process.env.REACT_APP_CONTEXT_KEY;
